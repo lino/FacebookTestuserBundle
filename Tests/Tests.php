0efc458db39b6da5599b4bcd877dc2c1fa2a34e0
Lino <l.helms@digitalpioneers.de>
2012-09-12 16:54:03 +0200

    Added Testing infrastructure (Travis, Tests, Composer)

---
+++ Tests/Tests.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace lino_dp\FacebookTestuserBundle;
+
+class BundleTests extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->markTestIncomplete('Test is included');
+    }
+}
